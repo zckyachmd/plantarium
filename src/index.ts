c4ebd4b70ebb4628f6b28753d5eebb5034a574b8
Zacky Achmad <zckyachmd@gmail.com>
2024-08-25 14:02:57 +0700

    add: api-spec.json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { Hono } from "hono";
+import { swaggerUI } from "@hono/swagger-ui";
+import { readFileSync } from "fs";
+import { join } from "path";
 import api from "./routes/api";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.get("/api", swaggerUI({ url: "/api/spec.json" }));
+app.get("/api/spec.json", (c) => {
+  try {
+    const filePath = join(process.cwd(), "public", "api-spec.json");
+    const jsonData = readFileSync(filePath, "utf-8");
+
+    return c.json(JSON.parse(jsonData));
+  } catch (error) {
+    return c.json({ error: "File not found or invalid JSON format" }, 500);
+  }
 });
 
 app.route("/api", api);
